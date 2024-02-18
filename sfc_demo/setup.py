826e0bf651f3eedf64c9123b9116c82b44ebc86a
Your Name <deckerwill7@gmail.com>
2024-02-18 10:47:11 -0600

    fix package stuff

--- sfc_demo/setup.py
+++ sfc_demo/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sfc-demo",
+    name="sfc_demo",
     version="0.0.1",
     author="Will Decker",
     author_email="deckerwill7@gmail.com",
     description="Tools for executing static functional connectivity analysis",
-    url="https://github.com/w-decker/sfc-demo/tree/main/sfc-demo",
+    url="https://github.com/w-decker/sfc-demo/tree/main/sfc_demo",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
