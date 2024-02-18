71fe280ab7e19d2ba02d4b21cdf0ad6a82ff067f
Your Name <deckerwill7@gmail.com>
2024-02-18 10:12:39 -0600

    use `find_packages()`

--- sfc-demo/setup.py
+++ sfc-demo/setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="deckerwill7@gmail.com",
     description="Tools for executing static functional connectivity analysis",
     url="https://github.com/w-decker/sfc-demo/tree/main/sfc-demo",
-    packages=['sfc-demo'],
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
